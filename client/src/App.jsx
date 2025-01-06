a4e0a286d202a5819851821367181f96b2f8fa4b
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2025-01-06 22:04:22 +0530

    signup page made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import SignupPage from '../pages/SignupPage';
+import LoginPage from '../pages/LoginPage';
 
 function App() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-yellow-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
-
+      <Routes>
+        <Route path='/' element={"Home"} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
     </div>
   )
 }
