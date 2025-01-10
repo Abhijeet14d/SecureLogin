44b23f03334d597b8b5c51c491d16a1f8fe6c79e
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2025-01-10 12:07:57 +0530

    version 1.1

--- server/index.js
+++ server/index.js
@@ -4,16 +4,26 @@ import express from "express";
 import authRoutes from "./routes/routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use(cors({origin: "http://localhost:5173", credentials:true}));
 
 app.use("/api/auth",authRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/client/dist")));
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    });
+}
 
 
 app.listen(PORT, ()=>{
